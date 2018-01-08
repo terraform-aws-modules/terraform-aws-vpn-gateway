a902052265d3613625bda4b35ed5cf7eed5e5397
Miguel Ferreira <miguelferreira@me.com>
2018-01-08 11:26:46 +0100

    Add examples

---
+++ examples/complete-vpn-gateway-with-static-routes/README.md
@@ -0,0 +1,19 @@
+Complete VPN Gateway Setup
+============
+
+Configuration in this directory creates set of VPN Gateway related resources which may be sufficient for staging or production environment (look into [minimal-vpn-gateway](../minimal-vpn-gateway) for more simplified setup).
+
+There will be a VPN Connection created linking a (pre-existing) VPN Gateway in a VPC to a (pre-existing) Customer Gateway, without automatic route propagation to selected VPC subnets enabled.
+
+Usage
+=====
+
+To run this example you need to execute:
+
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
+Run `terraform destroy` when you don't need these resources.
---
+++ examples/complete-vpn-gateway/outputs.tf
@@ -0,0 +1,27 @@
+output "vpn_connection_id" {
+  value = "${module.vpn_gateway.vpn_connection_id}"
+}
+
+output "vpn_connection_tunnel1_address" {
+  value = "${module.vpn_gateway.vpn_connection_tunnel1_address}"
+}
+
+output "vpn_connection_tunnel1_cgw_inside_address" {
+  value = "${module.vpn_gateway.vpn_connection_tunnel1_cgw_inside_address}"
+}
+
+output "vpn_connection_tunnel1_vgw_inside_address" {
+  value = "${module.vpn_gateway.vpn_connection_tunnel1_vgw_inside_address}"
+}
+
+output "vpn_connection_tunnel2_address" {
+  value = "${module.vpn_gateway.vpn_connection_tunnel2_address}"
+}
+
+output "vpn_connection_tunnel2_cgw_inside_address" {
+  value = "${module.vpn_gateway.vpn_connection_tunnel2_cgw_inside_address}"
+}
+
+output "vpn_connection_tunnel2_vgw_inside_address" {
+  value = "${module.vpn_gateway.vpn_connection_tunnel2_vgw_inside_address}"
+}
