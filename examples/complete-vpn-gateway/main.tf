072e7a863923249465d2f857730bde4e121f5f3b
Miguel Ferreira <miguelferreira@me.com>
2018-01-11 11:08:07 +0100

    Fix issue with computed resources used for count attribute (#4)
    * Fix variable name in example with satatic routes
    
    * Add variable vpc_subnet_route_table_count to workaround issue #10857
    
    * Use variable vpc_subnet_route_table_count in examples
    
    * Fix names and cgw IP in minimal example
    
    * Add example suffix to cgw name tag
    
    * Fix names, cgw IP and static routes in example

--- examples/complete-vpn-gateway/main.tf
+++ examples/complete-vpn-gateway/main.tf
@@ -1,10 +1,16 @@
+variable "vpc_private_subnets" {
+  default = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+}
+
 module "vpn_gateway" {
   source = "../../"
 
-  vpn_gateway_id                  = "${module.vpc.vgw_id}"
-  customer_gateway_id             = "${aws_customer_gateway.main.id}"
-  vpc_id                          = "${module.vpc.vpc_id}"
-  vpc_subnet_route_table_ids      = ["${module.vpc.private_route_table_ids}"]
+  vpn_gateway_id      = "${module.vpc.vgw_id}"
+  customer_gateway_id = "${aws_customer_gateway.main.id}"
+
+  vpc_id                       = "${module.vpc.vpc_id}"
+  vpc_subnet_route_table_ids   = ["${module.vpc.private_route_table_ids}"]
+  vpc_subnet_route_table_count = "${length(var.vpc_private_subnets)}"
 }
 
 resource "aws_customer_gateway" "main" {
@@ -13,7 +19,7 @@ resource "aws_customer_gateway" "main" {
   type       = "ipsec.1"
 
   tags {
-    Name = "main-customer-gateway"
+    Name = "main-customer-gateway-complete-example"
   }
 }
 
@@ -26,7 +32,7 @@ module "vpc" {
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-  public_subnets  = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+  public_subnets  = ["${var.vpc_private_subnets}"]
 
   enable_vpn_gateway = true
 
--- examples/minimal-vpn-gateway/main.tf
+++ examples/minimal-vpn-gateway/main.tf
@@ -1,34 +1,40 @@
+variable "vpc_private_subnets" {
+  default = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+}
+
 module "vpn_gateway" {
   source = "../../"
 
   create_vpn_connection = false
 
-  vpn_gateway_id             = "${module.vpc.vgw_id}"
-  customer_gateway_id        = "${aws_customer_gateway.main.id}"
-  vpc_id                     = "${module.vpc.vpc_id}"
-  vpc_subnet_route_table_ids = ["${module.vpc.private_route_table_ids}"]
+  vpn_gateway_id      = "${module.vpc.vgw_id}"
+  customer_gateway_id = "${aws_customer_gateway.main.id}"
+
+  vpc_id                       = "${module.vpc.vpc_id}"
+  vpc_subnet_route_table_ids   = ["${module.vpc.private_route_table_ids}"]
+  vpc_subnet_route_table_count = "${length(var.vpc_private_subnets)}"
 }
 
 resource "aws_customer_gateway" "main" {
   bgp_asn    = 65000
-  ip_address = "172.83.124.10"
+  ip_address = "172.83.124.12"
   type       = "ipsec.1"
 
   tags {
-    Name = "main-customer-gateway"
+    Name = "main-customer-gateway-minimal-example"
   }
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "complete-example"
+  name = "minimal-example"
 
   cidr = "10.10.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-  public_subnets  = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+  public_subnets  = ["${var.vpc_private_subnets}"]
 
   enable_vpn_gateway = true
 
