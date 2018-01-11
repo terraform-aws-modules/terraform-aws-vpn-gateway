f6defcb67160c1f887616adfb075fbffea728ddb
Miguel Ferreira <miguelferreira@me.com>
2018-01-11 08:55:34 +0100

    Fix variable name in example with satatic routes

--- examples/complete-vpn-gateway-with-static-routes/main.tf
+++ examples/complete-vpn-gateway-with-static-routes/main.tf
@@ -6,8 +6,9 @@ module "vpn_gateway" {
 
   vpn_gateway_id      = "${module.vpc.vgw_id}"
   customer_gateway_id = "${aws_customer_gateway.main.id}"
-  vpc_id              = "${module.vpc.vpc_id}"
-  vpc_subnet_ids      = ["${module.vpc.private_route_table_ids}"]
+
+  vpc_id                     = "${module.vpc.vpc_id}"
+  vpc_subnet_route_table_ids = ["${module.vpc.private_route_table_ids}"]
 }
 
 resource "aws_customer_gateway" "main" {
