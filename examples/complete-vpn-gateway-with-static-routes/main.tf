148a07d216a835f9f605801e18417b4cf977b45e
Lowe Schmidt <github@loweschmidt.se>
2019-08-15 14:50:14 +0200

    Fixes #23

--- examples/complete-vpn-gateway-with-static-routes/main.tf
+++ examples/complete-vpn-gateway-with-static-routes/main.tf
@@ -7,6 +7,11 @@ variable "vpc_private_subnets" {
   default = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
 }
 
+variable "vpc_public_subnets" {
+  type    = list(string)
+  default = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+}
+
 module "vpn_gateway" {
   source = "../../"
 
@@ -46,8 +51,8 @@ module "vpc" {
   cidr = "10.10.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-  public_subnets  = var.vpc_private_subnets
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
 
   enable_nat_gateway = false
 
