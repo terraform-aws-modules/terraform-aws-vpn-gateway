b3338d38d96b8ac735f647c695618ccbe09b3bd9
Bryant Biggs <bryantbiggs@gmail.com>
2022-11-12 08:52:47 -0500

    fix: Add fix to CI workflow

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.31"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.43"
+      version = ">= 4.0"
     }
   }
 }
