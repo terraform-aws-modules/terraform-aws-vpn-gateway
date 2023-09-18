5605acbdf75b01174b983b8f250555c7a1e53a3d
Anton Babenko <anton@antonbabenko.com>
2023-09-18 09:13:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -476,7 +476,7 @@ resource "aws_vpn_connection_route" "default" {
 
 ### Fix tagging on Transit Gateway Attachment if it exists
 resource "aws_ec2_tag" "tags" {
-  for_each = { for key, value in merge({ "Name" = local.name_tag }, var.tags) : key => value if var.connect_to_transit_gateway == true }
+  for_each = { for key, value in merge({ "Name" = local.name_tag }, var.tags) : key => value if var.create_vpn_connection && var.connect_to_transit_gateway }
 
   resource_id = try(
     aws_vpn_connection.default[0].transit_gateway_attachment_id,
