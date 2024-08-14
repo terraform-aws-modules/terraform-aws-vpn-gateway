c9b9b6c3ef554f674fecaeea402d0e6b70c09591
James Martinez <james-martinez@users.noreply.github.com>
2024-08-14 16:18:51 +0000

    fmt

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_vpn_connection" "default" {
   customer_gateway_id = var.customer_gateway_id
   type                = "ipsec.1"
 
-  static_routes_only = var.vpn_connection_static_routes_only
+  static_routes_only  = var.vpn_connection_static_routes_only
   enable_acceleration = var.vpn_connection_enable_acceleration
 
   tunnel1_phase1_dh_group_numbers = var.tunnel1_phase1_dh_group_numbers
@@ -133,7 +133,7 @@ resource "aws_vpn_connection" "tunnel" {
   customer_gateway_id = var.customer_gateway_id
   type                = "ipsec.1"
 
-  static_routes_only = var.vpn_connection_static_routes_only
+  static_routes_only  = var.vpn_connection_static_routes_only
   enable_acceleration = var.vpn_connection_enable_acceleration
 
   tunnel1_inside_cidr = var.tunnel1_inside_cidr
@@ -245,7 +245,7 @@ resource "aws_vpn_connection" "preshared" {
   customer_gateway_id = var.customer_gateway_id
   type                = "ipsec.1"
 
-  static_routes_only = var.vpn_connection_static_routes_only
+  static_routes_only  = var.vpn_connection_static_routes_only
   enable_acceleration = var.vpn_connection_enable_acceleration
 
   tunnel1_preshared_key = var.tunnel1_preshared_key
@@ -354,7 +354,7 @@ resource "aws_vpn_connection" "tunnel_preshared" {
   customer_gateway_id = var.customer_gateway_id
   type                = "ipsec.1"
 
-  static_routes_only = var.vpn_connection_static_routes_only
+  static_routes_only  = var.vpn_connection_static_routes_only
   enable_acceleration = var.vpn_connection_enable_acceleration
 
   tunnel1_inside_cidr = var.tunnel1_inside_cidr
